b17fc2e9718f2c80e8d5505e6f669852fb431e28
gooriiie <fkstldghks123@naver.com>
2021-06-27 17:45:50 +0900

    백준 18870번

---
+++ CoordinateSort.py
@@ -0,0 +1,18 @@
+import sys
+from bisect import bisect_left, bisect_right
+
+N = int(input())
+
+coordinate = list(map(int, sys.stdin.readline().rstrip().split()))
+
+# 2 4 -10 4 -9
+# 1000 999 1000 999 1000 999
+
+sortedCoordinate = sorted(list(set(coordinate)))
+
+# -10 -9 2 4 4
+# 999 999 999 1000 1000 1000
+# 999 1000
+for i in range(N):
+    index = bisect_left(sortedCoordinate, coordinate[i])
+    print(index, end=" ")
